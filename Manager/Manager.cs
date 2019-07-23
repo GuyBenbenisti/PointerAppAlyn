64ea92f55da3608ef3dbee15fa77a3bd952fa6a6
Guy Benbenisti <t-gubenb@microsoft.com>
2019-07-23 10:39:58 +0300

    Currently ObjectDetector uses Azure computer vision.

---
+++ Manager/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tobii.Interaction;
+
+namespace Manager
+{
+    class Manager
+    {
+        private TobiiAgent.TobiiAgent m_Agent = new TobiiAgent.TobiiAgent(new Host());
+        private VideoAppAndObjectDetector.ObjectDetector m_Detector;
+        private void TryAndRecognize(double i_PointX, double i_PointY)
+        {
+            //TODO: Implement
+            throw new Exception("Not implemented");
+        }
+    }
+}
