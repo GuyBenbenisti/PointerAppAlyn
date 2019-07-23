68ff1b1b181ffd2e8f9093e4de214cf8f0fdc794
Shahar Prish <shaharp@microsoft.com>
2019-07-23 11:32:09 +0300

    Yolo console pipe implemented.

---
+++ YoloPipe.Harness/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoloPipe.Harness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestYoloPipe();
+            TestYoloJsonPipe();
+            Console.ReadLine();
+        }
+
+        private static async Task TestYoloJsonPipe()
+        {
+            var pipe = new YoloJsonPipe("cmd.exe", "/c type c:\\temp\\sim.txt");
+            pipe.Start();
+            int i = 0;
+            while (true)
+            {
+                var obj = await pipe.ReadJsonObjextAsync();
+                if (obj != null)
+                {
+                    i++;
+                    Console.WriteLine($"Read {obj.Length} chars. Object #{i}");
+                }
+            }
+        }
+
+        private static async Task TestYoloPipe()
+        {
+            // var pipe = new YoloPipe("cmd.exe", "/c dir");
+            var pipe = new YoloPipe("cmd.exe", "/c type c:\\temp\\sim.txt");
+            pipe.Start();
+            await DoIt(pipe);
+        }
+
+        private static async Task DoIt(YoloPipe pipe)
+        {
+            string line = null;
+            do
+            {
+                line = await pipe.ReadNext();
+                Console.WriteLine(line);
+            } while (line != null);
+        }
+    }
+}
---
+++ YoloPipe/YoloJsonPipe.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoloPipe
+{
+    public class YoloJsonPipe
+    {
+        private YoloPipe _pipe;
+
+        public YoloJsonPipe(string exe, string arguments)
+        {
+            _pipe = new YoloPipe(exe, arguments);
+
+        }
+
+        public void Start()
+        {
+            _pipe.Start();
+        }
+
+        public async Task<string> ReadJsonObjextAsync()
+        {
+            StringBuilder b = new StringBuilder();
+            string line;
+            do
+            {
+                line = await _pipe.ReadNext();
+                if (line != "#")
+                {
+                    b.Append(line);
+                }
+            } while (line != "#" && line != null);
+
+            var result = b.ToString();
+            if (result == "")
+            {
+                result = null;
+            }
+
+            return result;
+        }
+    }
+}
