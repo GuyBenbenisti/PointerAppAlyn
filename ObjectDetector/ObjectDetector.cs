98ef4c8d6a2648a0a492427ae017cff8e5495e41
Guy Benbenisti <t-gubenb@microsoft.com>
2019-07-24 11:57:21 +0300

    Added feature to capture frame on detection, save it locally than send the image to ComputerVision for objects detection.

--- ObjectDetector/ObjectDetector.cs
+++ ObjectDetector/ObjectDetector.cs
@@ -1,46 +1,29 @@
 ﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace UI
 {
-   public class ObjectDetector
+    public class ObjectDetector
     {
         // subscriptionKey = "0123456789abcdef0123456789ABCDEF"
         private const string m_SubscriptionKey = "f00b15e35dc54148aed210c0611136f5";
-        private MemoryStream m_PictureMemoryStream;
-        private string localImagePath = @"C:\Documents\LocalImage.jpg";
-        // private const string localImagePath = @"Path";
 
-        private void detect()
+        public async void Detect(MemoryStream i_MS)
         {
             ComputerVisionClient computerVision = new ComputerVisionClient(
                 new ApiKeyServiceClientCredentials(m_SubscriptionKey),
                 new System.Net.Http.DelegatingHandler[] { });
 
-            // You must use the same region as you used to get your subscription
-            // keys. For example, if you got your subscription keys from westus,
-            // replace "westcentralus" with "westus".
-            //
-            // Free trial subscription keys are generated in the "westus"
-            // region. If you use a free trial subscription key, you shouldn't
-            // need to change the region.
-
             // Specify the Azure region
             computerVision.Endpoint = "https://westcentralus.api.cognitive.microsoft.com";
 
-            Console.WriteLine("Images being analyzed ...");
-            //var t1 = AnalyzeRemoteAsync(computerVision, remoteImageUrl);
-            var t2 = AnalyzeLocalAsync(computerVision, localImagePath);
-
-            Task.WhenAll(t2).Wait(5000);
-            Console.WriteLine("Press ENTER to exit");
-            Console.ReadLine();
+            string localImagePath = @"C:\Users\t-gubenb\Desktop\Test\TobiiCapture.jpg";
+            using(Stream imagestream = i_MS)
+            {
+               var analysis = AnalyzeLocalAsync(computerVision, localImagePath);
+            }
         }
 
         private static async Task AnalyzeLocalAsync(
@@ -48,19 +31,15 @@ namespace UI
         {
             if (!File.Exists(imagePath))
             {
-                Console.WriteLine(
-                    "\nUnable to open or read localImagePath:\n{0} \n", imagePath);
                 return;
             }
 
             using (Stream imageStream = File.OpenRead(imagePath))
             {
                 DetectResult analysis = await computerVision.DetectObjectsInStreamAsync(imageStream);
-                //imageStream, features);
-                //DisplayResults(analysis, imagePath);
                 foreach (DetectedObject obj in analysis.Objects)
                 {
-                    Console.WriteLine("ObjectProperty: {0}, Confidence: {1},", obj.ObjectProperty, obj.Confidence);
+                    //Console.WriteLine("ObjectProperty: {0}, Confidence: {1},", obj.ObjectProperty, obj.Confidence);
                 }
             }
         }
